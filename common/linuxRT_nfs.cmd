44ba8d3a47b2e1d91415dfa243acb0b2df27e81d
Bruce Hill <bhill@slac.stanford.edu>
2015-07-14 22:25:31 +0000

    Removed unneeded busybox prefix to mount commands
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/iocCommon/linuxRT@21847 ececa60f-9f63-0410-97c7-ea05888eb260

--- common/linuxRT_nfs.cmd
+++ common/linuxRT_nfs.cmd
@@ -6,7 +6,7 @@
 # Mount the IOC data area so that the IOC can write files.
 # =============================================================
 mkdir -p /reg/d/iocData
-busybox mount -o nolock,rw,hard,intr,vers=3 -t nfs 172.21.32.20:/nfsexport/iocData   /reg/d/iocData
+mount -o nolock,rw,hard,intr,vers=3 -t nfs 172.21.32.20:/nfsexport/iocData   /reg/d/iocData
 
 # =============================================================
 # Note: /reg/d/iocCommon has already been mounted via ipxe.ini so
@@ -18,14 +18,14 @@ busybox mount -o nolock,rw,hard,intr,vers=3 -t nfs 172.21.32.20:/nfsexport/iocDa
 # ==============================================================================
 
 mkdir -p  /reg/neh/home
-busybox mount -o nolock,ro,hard,intr,vers=3 -t nfs 172.21.47.28:/nfsexport/home   /reg/neh/home
+mount -o nolock,ro,hard,intr,vers=3 -t nfs 172.21.47.28:/nfsexport/home   /reg/neh/home
 
 mkdir -p  /reg/neh/home1
 mount -o nolock,ro,hard,intr,vers=3 -t nfs 172.21.47.28:/u2/users         /reg/neh/home1
 ln -s /reg/neh/home1 /home1
 
 mkdir -p  /reg/neh/home2
-busybox mount -o nolock,rw,hard,intr,vers=3 -t nfs 172.21.47.25:/u2/users         /reg/neh/home2
+mount -o nolock,ro,hard,intr,vers=3 -t nfs 172.21.47.25:/u2/users         /reg/neh/home2
 ln -s /reg/neh/home2 /home2
 
 # =============================================================
@@ -33,9 +33,9 @@ ln -s /reg/neh/home2 /home2
 # =============================================================
 
 mkdir -p /reg/g/pcds
-busybox mount -o nolock,rw,hard,intr,vers=3 -t nfs 172.21.32.28:/nfsexport/pcds   /reg/g/pcds
+mount -o nolock,ro,hard,intr,vers=3 -t nfs 172.21.32.28:/nfsexport/pcds   /reg/g/pcds
 
 mkdir -p /reg/common/package
-busybox mount -o nolock,ro,hard,intr,vers=3 -t nfs 172.21.32.28:/nfsexport/package   /reg/common/package
+mount -o nolock,ro,hard,intr,vers=3 -t nfs 172.21.32.28:/nfsexport/package   /reg/common/package
 
 # End of file
