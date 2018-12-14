db79a428242a1e1408c1bff12bf0b414131abd91
Bruce Hill <bhill@slac.stanford.edu>
2018-12-13 20:24:11 -0800

    For apalis we need to set a passwd manually

--- common/add_users.cmd
+++ common/add_users.cmd
@@ -37,20 +37,20 @@ addgroup -g 2341 ps-ioc
 
 # ====================================================================
 # Create one userid for each hutch or beamline area
-adduser -G ps-ioc -u 10404 amoioc -s $USER_SHELL -D; passwd -d amoioc
-adduser -G ps-ioc -u 12958 auxioc -s $USER_SHELL -D; passwd -d auxioc
-adduser -G ps-ioc -u 10668 cxiioc -s $USER_SHELL -D; passwd -d cxiioc
-adduser -G ps-ioc -u 13087 detioc -s $USER_SHELL -D; passwd -d detioc
-adduser -G ps-ioc -u 10403 feeioc -s $USER_SHELL -D; passwd -d feeioc
-adduser -G ps-ioc -u 10664 lasioc -s $USER_SHELL -D; passwd -d lasioc
-adduser -G ps-ioc -u 10669 mecioc -s $USER_SHELL -D; passwd -d mecioc
-adduser -G ps-ioc -u 10665 sxrioc -s $USER_SHELL -D; passwd -d sxrioc
-adduser -G ps-ioc -u 11926 thzioc -s $USER_SHELL -D; passwd -d thzioc
-adduser -G ps-ioc -u 11009 tstioc -s $BASH_SHELL -D; passwd -d tstioc
-adduser -G ps-ioc -u 10667 xcsioc -s $USER_SHELL -D; passwd -d xcsioc
-adduser -G ps-ioc -u 10666 xppioc -s $USER_SHELL -D; passwd -d xppioc
+adduser -G ps-ioc -u 10404 amoioc -s $USER_SHELL -D; passwd amoioc
+adduser -G ps-ioc -u 12958 auxioc -s $USER_SHELL -D; passwd auxioc
+adduser -G ps-ioc -u 10668 cxiioc -s $USER_SHELL -D; passwd cxiioc
+adduser -G ps-ioc -u 13087 detioc -s $USER_SHELL -D; passwd detioc
+adduser -G ps-ioc -u 10403 feeioc -s $USER_SHELL -D; passwd feeioc
+adduser -G ps-ioc -u 10664 lasioc -s $USER_SHELL -D; passwd lasioc
+adduser -G ps-ioc -u 10669 mecioc -s $USER_SHELL -D; passwd mecioc
+adduser -G ps-ioc -u 10665 sxrioc -s $USER_SHELL -D; passwd sxrioc
+adduser -G ps-ioc -u 11926 thzioc -s $USER_SHELL -D; passwd thzioc
+adduser -G ps-ioc -u 11009 tstioc -s $BASH_SHELL -D; passwd tstioc
+adduser -G ps-ioc -u 10667 xcsioc -s $USER_SHELL -D; passwd xcsioc
+adduser -G ps-ioc -u 10666 xppioc -s $USER_SHELL -D; passwd xppioc
 
 # Test only
-adduser -G ps-ioc -u 10174 bhill -s $BASH_SHELL -D -h /home5/bhill; passwd -d bhill
+adduser -G ps-ioc -u 10174 bhill -s $BASH_SHELL -D -h /home5/bhill; passwd bhill slac
 
 # End of file
