da12d98719ce1345d3b4de14e8924879002da5a4
Bruce Hill <bhill@slac.stanford.edu>
2019-07-15 22:04:38 -0700

    Update default EDT driver to 5.5.1.6.
    Also cleaned up install checks to test for device first instead
    of checking for existing driver dir so we can flag devices w/ no driver.

--- common/kernel-modules.cmd
+++ common/kernel-modules.cmd
@@ -3,6 +3,12 @@
 # =========================================
 export TZ=PST8PDT
 
+if [ -z "$IOC_COMMON" ]; then
+	export IOC_COMMON=/reg/d/iocCommon
+fi
+if [ -z "$T_A" ]; then
+	export T_A=rhel7-x86_64
+fi
 # =========================================
 # Find the packages and driver paths, allowing override
 # of the default driver version or path
@@ -12,32 +18,28 @@ source $IOC_COMMON/$T_A/common/kernel-module-dirs.cmd
 # =================================================
 # Install the kernel drivers for installed hardware
 # =================================================
-if [ -d $EDT_DRIVER/ ];
-then
-	lspci_edt=`lspci -d 123d:* -n`
-	if [ "$lspci_edt" != "" ];
-	then
+lspci_edt=`lspci -d 123d:* -n`
+if [ "$lspci_edt" != "" ]; then
+	if [ -d $EDT_DRIVER/ ]; then
 		echo Installing EDT driver: $EDT_DRIVER
 		mkdir -p /opt/EDTpdv
 		mount --bind $EDT_DRIVER /opt/EDTpdv
-		/opt/EDTpdv/edtinit start
+		/opt/EDTpdv/edtinit.sh start
 	else
-		echo EDT framegrabber not found.
+		echo EDT driver dir not found: $EDT_DRIVER
 	fi
 else
-	echo EDT driver dir not found: $EDT_DRIVER
+	echo EDT framegrabber not found.
 fi
 
-if [ -d $EVENT2_DRIVER/ ];
-then
-	# Check for MRF EVR boards w/ PLX 9030 bridge
-	lspci_mrf_evr=`lspci -d 10b5:9030 -n`
-	# Also check for SLAC EVR
-	lspci_slac_evr=`lspci -d 1a4a:2010 -n`
-	if [ "$lspci_mrf_evr" != "" -o "$lspci_slac_evr" != "" ];
-	then
-		echo Installing EVENT2 driver: $EVENT2_DRIVER
-		$EVENT2_DRIVER/driver/evr_load_module
+# Check for MRF EVR boards w/ PLX 9030 bridge
+lspci_mrf_evr=`lspci -d 10b5:9030 -n`
+# Also check for SLAC EVR
+lspci_slac_evr=`lspci -d 1a4a:2010 -n`
+if [ "$lspci_mrf_evr" != "" -o "$lspci_slac_evr" != "" ]; then
+	if [ -n "$EV2_DRIVER" -a -d $EV2_DRIVER/ ]; then
+		echo Installing EVENT2 driver: $EV2_DRIVER
+		$EV2_DRIVER/driver/evr_load_module
 		if [ -e /dev/era0 ];
 		then
 			chmod a+rw /dev/er*
@@ -51,13 +53,44 @@ then
 			/sbin/rmmod pci_mrfevg
 		fi
 	else
-		echo EVR not found.
+		echo event2 driver dir not found: $EV2_DRIVER
 	fi
+else
+	echo EVR not found.
+fi
+
+lspci_perle=`lspci -d 155f:* -n`
+if [ "$lspci_perle" != "" ]; then
+	if [ -n $PERLE_SERIAL_DRIVER -a -d $PERLE_SERIAL_DRIVER/ ]; then
+		echo Installing PERLE driver: $PERLE_SERIAL_DRIVER
+		insmod $PERLE_SERIAL_DRIVER/perle-serial.ko
+	else
+		echo Perle Serial driver dir not found: $PERLE_SERIAL_DRIVER
+	fi
+else
+	echo Perle device not found.
+fi
+
+#lspci_megaraid=`lspci -d 1000:005d -n`
+#if [ "$lspci_megaraid" != "" ]; then
+#	if [ -n $MEGARAID_DRIVER -a -d $MEGARAID_DRIVER/ ]; then
+#		echo Installing MegaRaid driver: $MEGARAID_DRIVER
+#		insmod $MEGARAID_DRIVER/megaraid_sas.ko
+#
+#		#megaRAID command line utility
+#		ln -s $MEGARAID_DRIVER/storcli64 /usr/bin/storcli64
+#	else
+#		echo Megaraid driver dir not found: $MEGARAID_DRIVER
+#	fi
+#else
+#	echo MegaRaid device not found.
+#fi
+
 fi
 
 # Cleanup env so defaults won't stick during debugging
 unset EDT_DRIVER;			unset EDT_VER
-unset EVENT2_DRIVER;		unset EVENT2_VER
+unset EV2_DRIVER;			unset EV2_VER;
 unset MEGARAID_DRIVER;		unset MEGARAID_VER
 unset PERLE_SERIAL_DRIVER;	unset PERLE_VER
 
