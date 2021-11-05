1c971f9b1d8010a7748622b84c64d5cbe8300da3
Bruce Hill <bhill@slac.stanford.edu>
2021-11-04 18:42:08 -0700

    Update to edt 5.5.8.2 and script cleanup.

--- common/kernel-module-dirs.cmd
+++ common/kernel-module-dirs.cmd
@@ -1,20 +1,32 @@
+if [ -z "$EPICS_SITE_TOP" ]; then
+	EPICS_SITE_TOP=/reg/g/pcds/epics
+fi
+if [ -z "$PACKAGE_SITE_TOP" ]; then
+	PACKAGE_SITE_TOP=/reg/g/pcds/package
+fi
+
 # Select default EDT driver if not overridden
 if [ "$EDT_DRIVER" == "" ];
 then
 	if [ "$EDT_VER" == "" ];
 	then
-		EDT_VER=R5.4.5.1
+		#EDT_VER=R5.4.5.1
+		EDT_VER=R5.5.8.2
 	fi
 	#EDT_DRIVER=$PACKAGE_SITE_TOP/EDTpdv/$EDT_VER/$T_A
-	EDT_DRIVER=$PACKAGE_SITE_TOP/EDTpdv/$EDT_VER
+	#EDT_DRIVER=$PACKAGE_SITE_TOP/EDTpdv/$EDT_VER
+	EDT_DRIVER=$PACKAGE_SITE_TOP/EDTpdv/$EDT_VER/`uname -r`
 fi
+export EDT_DRIVER
 
-# Select default event2 driver if not overridden
-if [ "$EVENT2_DRIVER" == "" ];
+# Select default ev2 driver if not overridden
+if [ "$EV2_DRIVER" == "" ];
 then
-	if [ "$EVENT2_VER" == "" ];
+	if [ "$EV2_VER" == "" ];
 	then
-		EVENT2_VER=latest
+		EV2_VER=latest
 	fi
-	EVENT2_DRIVER=$EPICS_SITE_TOP/modules/event2/$EVENT2_VER/
+	EV2_DRIVER=$EPICS_SITE_TOP/R3.14.12-0.4.0/modules/ev2_driver/$EV2_VER/
 fi
+export EV2_DRIVER
+
--- common/kernel-modules.cmd
+++ common/kernel-modules.cmd
@@ -3,6 +3,12 @@
 # =========================================
 export TZ=PST8PDT
 
+if [ -z "$IOC_COMMON" ]; then
+	export IOC_COMMON=/reg/d/iocCommon
+fi
+if [ -z "$T_A" ]; then
+	export T_A=linux-x86_64
+fi
 # =========================================
 # Find the packages and driver paths, allowing override
 # of the default driver version or path
@@ -12,32 +18,31 @@ source $IOC_COMMON/$T_A/common/kernel-module-dirs.cmd
 # =================================================
 # Install the kernel drivers for installed hardware
 # =================================================
-if [ -d $EDT_DRIVER/ ];
-then
-	lspci_edt=`lspci -d 123d:* -n`
-	if [ "$lspci_edt" != "" ];
-	then
+lspci_edt=`lspci -d 123d:* -n`
+if [ "$lspci_edt" != "" ]; then
+	if [ -d $EDT_DRIVER/ -a ! -e /dev/edt0 -a $UID -eq 0 ]; then
 		echo Installing EDT driver: $EDT_DRIVER
-		mkdir -p /opt/EDTpdv
-		mount --bind $EDT_DRIVER /opt/EDTpdv
-		/opt/EDTpdv/edtinit start
+		if [ ! -e /opt/EDTpdv/version -o "R$(cat /opt/EDTpdv/version)" != "${EDT_VER}" ]; then
+			mkdir -p /opt/EDTpdv
+			mount --bind $EDT_DRIVER /opt/EDTpdv
+			mount --bind $PACKAGE_SITE_TOP/EDTpdv/${EDT_VER}/pdv /opt/pdv
+		fi
+		/opt/EDTpdv/edtinit.sh start
 	else
-		echo EDT framegrabber not found.
+		echo EDT driver dir not found: $EDT_DRIVER
 	fi
 else
-	echo EDT driver dir not found: $EDT_DRIVER
+	echo EDT framegrabber not found.
 fi
 
-if [ -d $EVENT2_DRIVER/ ];
-then
-	# Check for MRF EVR boards w/ PLX 9030 bridge
-	lspci_mrf_evr=`lspci -d 10b5:9030 -n`
-	# Also check for SLAC EVR
-	lspci_slac_evr=`lspci -d 1a4a:2010 -n`
-	if [ "$lspci_mrf_evr" != "" -o "$lspci_slac_evr" != "" ];
-	then
-		echo Installing EVENT2 driver: $EVENT2_DRIVER
-		$EVENT2_DRIVER/driver/evr_load_module
+# Check for MRF EVR boards w/ PLX 9030 bridge
+lspci_mrf_evr=`lspci -d 10b5:9030 -n`
+# Also check for SLAC EVR
+lspci_slac_evr=`lspci -d 1a4a:2010 -n`
+if [ "$lspci_mrf_evr" != "" -o "$lspci_slac_evr" != "" ]; then
+	if [ -n "$EV2_DRIVER" -a -d $EV2_DRIVER/ ]; then
+		echo Installing EVENT2 driver: $EV2_DRIVER
+		$EV2_DRIVER/driver/evr_load_module
 		if [ -e /dev/era0 ];
 		then
 			chmod a+rw /dev/er*
@@ -51,13 +56,16 @@ then
 			/sbin/rmmod pci_mrfevg
 		fi
 	else
-		echo EVR not found.
+		echo event2 driver dir not found: $EV2_DRIVER
 	fi
+else
+	echo EVR not found.
 fi
+
 
 # Cleanup env so defaults won't stick during debugging
 unset EDT_DRIVER;			unset EDT_VER
-unset EVENT2_DRIVER;		unset EVENT2_VER
+unset EV2_DRIVER;			unset EV2_VER;
 unset MEGARAID_DRIVER;		unset MEGARAID_VER
 unset PERLE_SERIAL_DRIVER;	unset PERLE_VER
 
